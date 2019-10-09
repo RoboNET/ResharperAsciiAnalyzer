d74fb3820ad7e27e4dfe372c68186862e044d28f
Michael Korolev <michael443959@gmail.com>
2019-10-09 19:45:05 +0300

    Initial version of plugin

---
+++ src/dotnet/ReSharperPlugin.ResharperAsciiAnalyzer.Tests/TestEnvironment.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.ResharperAsciiAnalyzer.Tests
+{
+  [ZoneDefinition]
+  public interface IResharperAsciiAnalyzerTestZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>
+  {
+  }
+
+  [SetUpFixture]
+  public class TestEnvironment : ExtensionTestEnvironmentAssembly<IResharperAsciiAnalyzerTestZone>
+  {
+  }
+}
